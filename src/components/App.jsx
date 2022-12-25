c85c2582c6045c9d5ad83ea699820cb27d96e4b8
Aditya Lahane <adityalahane31@gmail.com>
2022-12-26 02:36:04 +0530

    Added notes using Arrow Function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
 import React from "react"; 
 import Footer from "./Footer";
 import Header from "./Header";
-import Note from "./Note";
+import Note from "./Note"; 
+import notes from "../notes";
 function App() {
     return <div>
         <Header/>
         <Footer/>
-        <Note/>
+        {notes.map(note=>(
+            <Note
+                key={note.key}
+                title={note.title}
+                content={note.content}
+            />
+        ))}
     </div>
 }
 
