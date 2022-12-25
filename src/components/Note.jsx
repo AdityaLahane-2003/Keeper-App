6f46812fc1fa144fd3418b0932fb3c5b5958c7d4
Aditya Lahane <adityalahane31@gmail.com>
2022-12-26 02:25:16 +0530

    Initial Commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note() {
+    return <div className="note">
+        <h1>This is Heading </h1>
+        <p>This is my first Note</p>
+    </div>
+} 
+
+export default Note;
